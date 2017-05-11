7bf4a1c551b486a881d57b28827729613551e6e3
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2017-05-11 12:08:14 +0100

    removed adding of modules

--- install_micropython.sh
+++ install_micropython.sh
@@ -77,24 +77,6 @@ function question { printf "${GREEN} > Do you want to build the unix port?${NC}"
 function answer_yes { cd $MPY_DIR/unix; make axtls && make; cd ../../; }
 ask question answer_yes
 
-# Ask if my modules should be added to the python build
-MPY_MOD=$MPY_DIR/esp8266/modules
-MAX_MOD=/media/psf/Home/Documents/Git/esp8266/maxi
-function question { printf "${GREEN} > Do you want to add Maxi's custom modules?${NC}"; }
-function answer_yes {
-	if [[ -d $MPY_MOD/maxi ]]; then
-		printf "${GREEN} > ${YELLOW}${MPY_MOD}/maxi${GREEN} already exists.${NC}\n"
-	else
-		if [[ -d $MAX_MOD ]]; then
-			ln -s $MAX_MOD $MPY_MOD
-			printf "${GREEN} > You need to re-compile the firmware!${NC}\n"
-		else
-			printf "${RED} > Could not find ${YELLOW}$MAX_MOD${RED}!${NC}\n"
-		fi
-	fi
-}
-ask question answer_yes
-
 # Ask if the firmware should be flashed
 MPY_FW=$MPY_DIR/esp8266/build/firmware-combined.bin
 
